15ae414422aa961b8cf2c50338392e60250a2038
Zhixing (Jeffrey) Yao <jeffreyyao@outlook.com>
2022-11-03 14:08:41 -0600

    mod

--- widgets/Fonts.py
+++ widgets/Fonts.py
@@ -1,5 +1,5 @@
 from PyQt5.QtGui import QFont
 
-font_title = QFont("Arial", 20, QFont.Weight.Bold)
-font_subtitle = QFont("Arial", 16, QFont.Weight.DemiBold)
-font_regular = QFont("Arial", 13, QFont.Weight.Normal)
+font_title = QFont("Helvetica", 20, QFont.Weight.Bold)
+font_subtitle = QFont("Helvetica", 14, QFont.Weight.DemiBold)
+font_regular = QFont("Helvetica", 11, QFont.Weight.Normal)
