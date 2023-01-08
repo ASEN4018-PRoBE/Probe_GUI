06a3707684b59508f4ba837800c448ea4d783bba
Zhixing Jeffrey Yao <jeffreyyao@outlook.com>
2023-01-07 23:58:51 -0700

    update

--- widgets/NavigationPane.py
+++ widgets/NavigationPane.py
@@ -14,15 +14,15 @@ class NavigationPane(QtWidgets.QWidget):
         self.btn_configuration = QtWidgets.QLabel("Configuration")
         self.btn_configuration.setFixedHeight(80)
         self.btn_configuration.setAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
-        self.btn_configuration.setFont(font_title)
+        self.btn_configuration.setFont(font_subtitle)
         self.btn_test_results = QtWidgets.QLabel("Test Results")
         self.btn_test_results.setFixedHeight(80)
         self.btn_test_results.setAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
-        self.btn_test_results.setFont(font_title)
+        self.btn_test_results.setFont(font_subtitle)
         self.btn_detailed_plots = QtWidgets.QLabel("Detailed Plots")
         self.btn_detailed_plots.setFixedHeight(80)
         self.btn_detailed_plots.setAlignment(Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
-        self.btn_detailed_plots.setFont(font_title)
+        self.btn_detailed_plots.setFont(font_subtitle)
         
         vbox_main.addWidget(self.btn_configuration)
         vbox_main.addWidget(self.btn_test_results)
@@ -32,20 +32,15 @@ class NavigationPane(QtWidgets.QWidget):
         hbox_btn = QtWidgets.QHBoxLayout()
         self.btn_start = QtWidgets.QPushButton()
         self.btn_start.setIcon(QtGui.QIcon("images/play.svg"))
-        self.btn_pause_resume = QtWidgets.QPushButton()
-        self.btn_pause_resume.setIcon(QtGui.QIcon("images/pause_resume.svg"))
         self.btn_stop = QtWidgets.QPushButton()
         self.btn_stop.setIcon(QtGui.QIcon("images/stop.svg"))
-        self.btn_stop.setFont(font_subtitle)
         hbox_btn.addWidget(self.btn_start)
-        hbox_btn.addWidget(self.btn_pause_resume)
         hbox_btn.addWidget(self.btn_stop)
         vbox_main.addLayout(hbox_btn)
         self.btn_start.setFixedHeight(55)
-        self.btn_pause_resume.setFixedHeight(55)
         self.btn_stop.setFixedHeight(55)
 
-        self.setStyleSheet('''
+        self.setStyleSheet(f'''
             QPushButton{{
                 font-size: 25px;
                 border: 1px solid gray;
@@ -54,9 +49,9 @@ class NavigationPane(QtWidgets.QWidget):
                 text-align: center;
             }}
             QLabel::hover{{
-                background: rgb({0},{1},{2})
+                background: rgb({light.red()},{light.green()},{light.blue()})
             }}
-        '''.format(light.red(),light.green(),light.blue()))
+        ''')
 
     def recolor(self, index, base, light):
         self.btn_configuration.setStyleSheet("QLabel{{background: rgb({0},{1},{2}}})".format(base.red(),base.green(),base.blue()))
