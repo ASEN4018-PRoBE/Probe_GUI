500c79db38d43fde271ea154b7c9ee515703ddc9
Zhixing Jeffrey Yao <jeffreyyao@outlook.com>
2023-01-13 22:26:42 -0700

    update

--- widgets/Fonts.py
+++ widgets/Fonts.py
@@ -1,14 +1,15 @@
+from sys import platform
 from PyQt6.QtGui import QFont
 
-# mac
-name = "Arial"
-font_title = QFont(name, 18, QFont.Weight.Bold)
-font_subtitle = QFont(name, 16, QFont.Weight.DemiBold)
-font_regular = QFont(name, 13, QFont.Weight.Normal)
-font_regular_bold = QFont(name, 13, QFont.Weight.Bold)
-
-# windows
-# font_title = QFont("Calibri", 18, QFont.Weight.Bold)
-# font_subtitle = QFont("Calibri", 14, QFont.Weight.DemiBold)
-# font_regular = QFont("Calibri", 10, QFont.Weight.Normal)
-# font_regular_bold = QFont("Calibri", 10, QFont.Weight.Bold)
+if platform == "darwin":
+    name = "Arial"
+    font_title = QFont(name, 18, QFont.Weight.Bold)
+    font_subtitle = QFont(name, 16, QFont.Weight.DemiBold)
+    font_regular = QFont(name, 13, QFont.Weight.Normal)
+    font_regular_bold = QFont(name, 13, QFont.Weight.Bold)
+elif platform == "win32":
+    # Windows...
+    font_title = QFont("Calibri", 18, QFont.Weight.Bold)
+    font_subtitle = QFont("Calibri", 14, QFont.Weight.DemiBold)
+    font_regular = QFont("Calibri", 10, QFont.Weight.Normal)
+    font_regular_bold = QFont("Calibri", 10, QFont.Weight.Bold)
