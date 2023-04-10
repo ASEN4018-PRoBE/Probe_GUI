f90dbb77e1cf22c6c035826a91974b6a31253977
Zhixing Jeffrey Yao <jeffreyyao@outlook.com>
2023-04-09 18:35:48 -0600

    update

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from widgets.MainWindow import MainWindow
 if __name__ == "__main__":
     App = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
-    window.resize(1200,840)
+    window.resize(1100,770)
     window.show()
     sys.exit(App.exec())
